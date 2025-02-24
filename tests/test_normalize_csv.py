eb8e344a8ea62864607de9942807f924850663a9
oatmeelsquares <142955426+oatmeelsquares@users.noreply.github.com>
2025-02-24 12:58:57 -0500

    Revert "Revert "LAB-03""

---
+++ tests/test_normalize_csv.py
@@ -0,0 +1,32 @@
+''' This module tests the functionality of bin.normalize_csv.py
+    
+    Author: rn7ena@virginia.edu
+'''
+
+import sys
+sys.path.append('.')
+
+import pandas as pd
+import bin.normalize_csv
+
+def test_df():
+    ''' make sure read_gainers returns a pandas dataframe'''
+    df = bin.normalize_csv.read_gainers('ygainers.csv')
+    assert isinstance(df, pd.DataFrame), f'read_gainers failed to return a DataFrame: {type(df)}'
+
+def test_headers():
+    ''' make sure read_gainers returns a df with the correct headers'''
+    headers = ['symbol', 'price', 'price_change', 'price_percent_change']
+    df = bin.normalize_csv.read_gainers('ygainers.csv')
+    assert (df.columns == headers).all(), f'returned df with wrong headers: {df.columns}'
+
+def test_concat():
+    ''' make sure read_gainers returns dfs that can be
+        easily merged with the correct dimensions'''
+    df1 = bin.normalize_csv.read_gainers('wsjgainers.csv')
+    df2 = bin.normalize_csv.read_gainers('ygainers.csv')
+    num_rows = df1.shape[0] + df2.shape[0]
+    right_shape = (num_rows, 4)
+    df = pd.concat([df1, df2], axis = 0)
+
+    assert df.shape == right_shape, f'read_gainers returned uncompatible dataframes {str(df)}'
