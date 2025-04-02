18b2eeb5aa2e3bb1c17796618b950055ecba72c0
Efrain Olivares <99214069+EfrainOlivaresUVA@users.noreply.github.com>
2025-04-01 23:19:04 -0700

    Update version_tests.py

--- tests/version_tests.py
+++ tests/version_tests.py
@@ -4,5 +4,5 @@ def test_os():
     assert sys.platform == 'linux'
 
 def test_version():
-    good_versions = ['3.10', '3.11']
+    good_versions = ['3.10', '3.11'] # will have to update to match changes in validation.yml.  By the way, your AWS vm also uses 3.12, right?
     assert sys.version[:4] in good_versions
